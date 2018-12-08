17c1dfe08bd8537250ebc5476f88a12d53e76eb2
Woody Lucas <wlucas0311@gmail.com>
2018-12-08 16:51:11 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,17 +1,42 @@
-def get_first_name_of_season_winner(data, season)
-  # code here
+ def get_first_name_of_season_winner(data, season)
+   data[season].each do |individuals|
+     
+     individuals.each do |key, value|
+       if value == 'Winner'
+         contestant = individuals["name"]
+         return contestant.split(' ').first
+       end 
+     end 
+   end 
+  
+  
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  
+  data.each do |season, array|
+    
+    array.each do |individuals|
+      
+      individuals.each do |key, value|
+        if value == occupation 
+          return individuals["name"]
+        end 
+      end 
+    
+    end 
+  
+      
+  end 
+
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+
 end
 
 def get_occupation(data, hometown)
-  # code here
+  
 end
 
 def get_average_age_for_season(data, season)
